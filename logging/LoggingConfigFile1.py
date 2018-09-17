34d6c5db578b7600d1310db81bfa2ae51ddb9dbb
eveeye <103056560@qq.com>
2018-09-17 22:10:19 +0800

    Create LoggingConfigFile1.py
    Add LoggingConfigFile1.py

---
+++ logging/LoggingConfigFile1.py
@@ -0,0 +1,13 @@
+import logging
+import logging.config
+
+"""
+例1
+"""
+
+logging.config.fileConfig("logger.conf")
+logger = logging.getLogger("example01")
+
+logger.debug('This is debug message')
+logger.info('This is info message')
+logger.warning('This is warning message')
